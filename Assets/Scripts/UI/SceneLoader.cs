85aae3ba50091362a369c7016a31caaf62bdf7d6
Agostinho Felipe <agostinhofelipeneto@outlook.com>
2023-02-13 00:45:20 -0300

    Added Start, Credits and Quit functionalities to main menu, added fade between scene

--- Assets/Scripts/UI/SceneLoader.cs
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+
     public void QuitGame()
     {
         Application.Quit();
+        print("Quit!");
+    }
+
+    public void FadeToScene()
+    {
+        animator.SetTrigger("FadeOut");   
+    }
+
+    public void OnFadeComplete(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
     }
 }
